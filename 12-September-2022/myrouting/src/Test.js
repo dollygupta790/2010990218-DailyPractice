7d5475bcd4628bc4ee431a5cea7df4b08e3101af
Dolly <dolly0218.be20@chitkara.edu.in>
2022-09-18 19:39:44 +0530

    Practice

--- 12-September-2022/myrouting/src/Test.js
+++ 12-September-2022/myrouting/src/Test.js
@@ -1,18 +1,25 @@
 import React from 'react';
 
-import Test from './components/Test';
-import Abcd from  './components/Abcd';
-
-const App= () => {
-  return (
-    <div>
-      <Test/>
-      <Test/>
-      <Abcd/>  
-      <Abcd/>   
+ //import Test from './components/Test';
+// import Abcd from  './components/Abcd';
+import  './Test.css';
+// const App= () => {
+//   return (
+//     <div>
+//       <Test/>
+//       <Test/>
+//       <Abcd/>  
+//       <Abcd/>   
      
-    </div>
-  )
+//     </div>
+//   )
+// }
+const Test=()=>{
+  <div>
+    <h1> This is Index component,Heading One</h1>
+    <h2>This Index Component,Heading</h2>
+    <P>This INdex Component</P>
+  </div>
 }
 
-export default App;
+export default Test;
